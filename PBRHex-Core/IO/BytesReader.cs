5e769fa13489accb21ef66a9e75668645c5f59f1
Sean Miller <seanmiller999@gmail.com>
2023-10-18 21:29:22 -0700

    Create BytesReader text template

---
+++ PBRHex-Core/IO/BytesReader.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.IO;
+
+namespace PBRHex.Core.IO
+{
+    internal enum Endianness
+    {
+        LittleEndian,
+        BigEndian
+    }
+
+    internal interface IByteStream
+    {
+        byte[] ReadBytes(long offset, int count);
+    }
+
+    internal class BytesReader
+    {
+        private readonly IByteStream stream;
+        private readonly Endianness endianness;
+
+        internal BytesReader(IByteStream stream, Endianness endianness) {
+            this.stream = stream;
+            this.endianness = endianness;
+        }
+
+        internal byte ReadByte(long offset) {
+            byte[] bytes = stream.ReadBytes(offset, 1);
+            return bytes[0];
+        }
+
+        internal Int16 ReadInt16(long offset) {
+            byte[] bytes = stream.ReadBytes(offset, 2);
+            
+            return endianness == Endianness.BigEndian
+                ? BinaryPrimitives.ReadInt16BigEndian(bytes)
+                : BinaryPrimitives.ReadInt16LittleEndian(bytes);
+        }
+
+        internal Int32 ReadInt32(long offset) {
+            byte[] bytes = stream.ReadBytes(offset, 4);
+            
+            return endianness == Endianness.BigEndian
+                ? BinaryPrimitives.ReadInt32BigEndian(bytes)
+                : BinaryPrimitives.ReadInt32LittleEndian(bytes);
+        }
+
+        internal Int64 ReadInt64(long offset) {
+            byte[] bytes = stream.ReadBytes(offset, 8);
+            
+            return endianness == Endianness.BigEndian
+                ? BinaryPrimitives.ReadInt64BigEndian(bytes)
+                : BinaryPrimitives.ReadInt64LittleEndian(bytes);
+        }
+
+        internal UInt16 ReadUInt16(long offset) {
+            byte[] bytes = stream.ReadBytes(offset, 2);
+            
+            return endianness == Endianness.BigEndian
+                ? BinaryPrimitives.ReadUInt16BigEndian(bytes)
+                : BinaryPrimitives.ReadUInt16LittleEndian(bytes);
+        }
+
+        internal UInt32 ReadUInt32(long offset) {
+            byte[] bytes = stream.ReadBytes(offset, 4);
+            
+            return endianness == Endianness.BigEndian
+                ? BinaryPrimitives.ReadUInt32BigEndian(bytes)
+                : BinaryPrimitives.ReadUInt32LittleEndian(bytes);
+        }
+
+        internal UInt64 ReadUInt64(long offset) {
+            byte[] bytes = stream.ReadBytes(offset, 8);
+            
+            return endianness == Endianness.BigEndian
+                ? BinaryPrimitives.ReadUInt64BigEndian(bytes)
+                : BinaryPrimitives.ReadUInt64LittleEndian(bytes);
+        }
+
+        internal Single ReadSingle(long offset) {
+            byte[] bytes = stream.ReadBytes(offset, 4);
+            
+            return endianness == Endianness.BigEndian
+                ? BinaryPrimitives.ReadSingleBigEndian(bytes)
+                : BinaryPrimitives.ReadSingleLittleEndian(bytes);
+        }
+
+        internal Double ReadDouble(long offset) {
+            byte[] bytes = stream.ReadBytes(offset, 8);
+            
+            return endianness == Endianness.BigEndian
+                ? BinaryPrimitives.ReadDoubleBigEndian(bytes)
+                : BinaryPrimitives.ReadDoubleLittleEndian(bytes);
+        }
+
+    }
+}
